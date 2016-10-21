b01a31a78c0d4f972bd838c3c0d47c33acfc42c9
pvo125 <pvo125@gmail.com>
2016-10-21 15:57:37 +0700

    1

---
+++ MDK-ARM/output/startup_stm32f10x_md.d
@@ -0,0 +1,1 @@
+.\output\startup_stm32f10x_md.o: ..\CMSIS\DeviceSupport\ST\STM32F10x\startup\arm\startup_stm32f10x_md.s
--- output/core_cm3.d
+++
@@ -1,2 +0,0 @@
-.\output\core_cm3.o: CMSIS\CoreSupport\core_cm3.c
-.\output\core_cm3.o: E:\Keil\ARM\ARMCC\bin\..\include\stdint.h
